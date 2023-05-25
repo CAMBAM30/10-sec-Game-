fa6996c21436d53c32c560b9882d442437cb0d69
CAMBAM30 <cmcdondle@gmail.com>
2023-05-25 08:46:57 -0600

    add input box, check for true or false ans

--- app.js
+++ app.js
@@ -5,14 +5,18 @@ $(document).ready(function() {
     }
     var questionMaker = function () {
         var question = {};
-        var num1 = randomNumberGenerator (10);
-        var num2 = randomNumberGenerator(10);
-
+        var num1 = numberGenerator (10);
+        var num2 = numberGenerator(10);
         question.answer = num1 + num2;
         question.equation = String(num1) + " + " + String(num2);
         return question;
     }
     myQuestion = questionMaker();
-
     $('#equation').text(myQuestion.equation);
+    var rightAnswer = function (playerInput, answer) {
+        console.log(playerInput === answer);
+    }
+    $('#player-input').on('keyup', function () {
+        rightAnswer(Number($(this).val()), myQuestion.answer);
+    });
 });

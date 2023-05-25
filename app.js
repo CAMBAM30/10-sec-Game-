9d0eafbf359beb413ec018fe9e6ae4c8c49e6814
CAMBAM30 <cmcdondle@gmail.com>
2023-05-25 08:02:02 -0600

    Set up and math question generator

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         var num2 = randomNumberGenerator(10);
 
         question.answer = num1 + num2;
-        question.equation = String(num1) + " + " String(num2);
+        question.equation = String(num1) + " + " + String(num2);
         return question;
     }
     myQuestion = questionMaker();

b813aa5321bdbd0ead27c1c6a43881dfee527f14
CAMBAM30 <cmcdondle@gmail.com>
2023-05-24 23:20:03 -0600

    css check & html and js first step generator

---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    var myQuestion;
+    var numberGenerator = function (size) {
+        return Math.floor(Math.random() * size);
+    }
+    var questionMaker = function () {
+        var question = {};
+        var num1 = randomNumberGenerator (10);
+        var num2 = randomNumberGenerator(10);
+
+        question.answer = num1 + num2;
+        question.equation = String(num1) + " + " String(num2);
+        return question;
+    }
+    myQuestion = questionMaker();
+
+    $('#equation').text(myQuestion.equation);
+});

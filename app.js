671f6388e8a0c2b802b64f16f87df20d9249919c
CAMBAM30 <cmcdondle@gmail.com>
2023-05-25 19:38:41 -0600

    add time clock, count, & time and expiration

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     var myQuestion;
+    var timeRemaining = 10;
     var numberGenerator = function (size) {
         return Math.floor(Math.random() * size);
     }
@@ -11,6 +12,10 @@ $(document).ready(function() {
         question.equation = String(num1) + " + " + String(num2);
         return question;
     }
+    var fixTimeRemaining = function (amount) {
+        timeRemaining += amount;
+        $('#time-Remaining').text(timeRemaining);
+    }
     var makeNewQuestion = function () {
     myQuestion = questionMaker();
     $('#equation').text(myQuestion.equation);
@@ -19,10 +24,17 @@ $(document).ready(function() {
         if(playerInput === answer) {
             makeNewQuestion();
             $('#player-input').val('');
+            fixTimeRemaining(+1);
         }
     }
     $('#player-input').on('keyup', function () {
         rightAnswer(Number($(this).val()), myQuestion.answer);
     });
+    var interval = setInterval(function () {
+        fixTimeRemaining(-1);
+        if(timeRemaining === 0) {
+            clearInterval(interval);
+        }
+    }, 1000);
     makeNewQuestion();
 });

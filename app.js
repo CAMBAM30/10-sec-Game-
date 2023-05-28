6ce719bbfe1d94a30fd8f45760782f4b938dd08b
CAMBAM30 <cmcdondle@gmail.com>
2023-05-27 21:33:50 -0600

    fix formatting  add score

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 $(document).ready(function() {
     var myQuestion;
+    var interval;
     var timeRemaining = 10;
+    var Score = 0;
+
+    var fixTimeRemaining = function (amount) {
+        timeRemaining += amount;
+        $('#time-Remaining').text(timeRemaining);
+    };
+    var fixScore = function (amount) {
+        Score += amount;
+        $('#Score').text(Score);
+    };
+    var beginGame = function () {
+        if (!interval) {
+            if (timeRemaining === 0){
+                fixTimeRemaining(10);
+                fixScore(-Score);
+            }
+            interval = setInterval(function () {
+                fixTimeRemaining(-1);
+                if(timeRemaining === 0) {
+                    clearInterval(interval);
+                    interval = undefined;
+                }
+            }, 1000);
+        }
+    };
     var numberGenerator = function (size) {
         return Math.floor(Math.random() * size);
-    }
+    };
     var questionMaker = function () {
         var question = {};
         var num1 = numberGenerator (10);
@@ -11,11 +37,8 @@ $(document).ready(function() {
         question.answer = num1 + num2;
         question.equation = String(num1) + " + " + String(num2);
         return question;
-    }
-    var fixTimeRemaining = function (amount) {
-        timeRemaining += amount;
-        $('#time-Remaining').text(timeRemaining);
-    }
+    };
+ 
     var makeNewQuestion = function () {
     myQuestion = questionMaker();
     $('#equation').text(myQuestion.equation);
@@ -25,16 +48,12 @@ $(document).ready(function() {
             makeNewQuestion();
             $('#player-input').val('');
             fixTimeRemaining(+1);
+            fixScore(+1);
         }
-    }
+    };
     $('#player-input').on('keyup', function () {
+        beginGame();
         rightAnswer(Number($(this).val()), myQuestion.answer);
     });
-    var interval = setInterval(function () {
-        fixTimeRemaining(-1);
-        if(timeRemaining === 0) {
-            clearInterval(interval);
-        }
-    }, 1000);
     makeNewQuestion();
 });
